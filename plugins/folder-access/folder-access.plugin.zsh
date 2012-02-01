55f5dded514351c18182964a60c0e2958ae9e5ae
Ed James <ed.james.email@gmail.com>
2012-02-01 15:01:42 +0000

    added additional folder access helper for home folder

--- plugins/folder-access/folder-access.plugin.zsh
+++ plugins/folder-access/folder-access.plugin.zsh
@@ -1,4 +1,13 @@
-c() { cd ~/Development/$1; }
+# 
+# ~/
+# 
+h() { cd ~/$1; }
+_h() { _files -W ~/ -/; }
+compdef _h h
 
+# 
+# ~/Development
+# 
+c() { cd ~/Development/$1; }
 _c() { _files -W ~/Development -/; }
 compdef _c c
